c9823ecf1011ffb2216d993eb4dfae772a74c8fd
Pavel Zosim <pavel.zosim@protonmail.com>
2025-11-24 13:33:16 +0200

    Added two more lectures

---
+++ 09_itrerpolation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSLearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 40;
+            string name = "John";
+
+            Console.WriteLine("Name: " + name + ", Age: " + age); // String concatenation
+            Console.WriteLine($"Name: {name}, Age: {age}"); // String interpolation
+        }
+    }
+}
